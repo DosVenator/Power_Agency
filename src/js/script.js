dda7acd0de0f4825b679917627a5d8ab6f1451ca
DosVenator <wadocraft@gmail.com>
2024-11-30 18:43:55 +0200

    feat:comments_block_swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 const body = document.querySelector('body');
+
+const swiperTwo = new Swiper('.swiper_2', {
+    slidesPerView: 5,
+    watchSlidesProgress: true,
+});
+const swiperOne = new Swiper('.swiper_1', {
+    thumbs: {
+        swiper: swiperTwo,
+      },
+});
