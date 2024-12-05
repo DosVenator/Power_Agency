4d4b2591bd93b50a9b7ad86ccbf425f01d92fdd1
DosVenator <wadocraft@gmail.com>
2024-12-05 09:59:59 +0200

    feat:header_products_hw

--- src/js/script.js
+++ src/js/script.js
@@ -5,13 +5,17 @@ const swiperTwo = new Swiper('.swiper_2', {
     watchSlidesProgress: true,
 });
 const swiperOne = new Swiper('.swiper_1', {
+    initialSlide: 2,
     thumbs: {
         swiper: swiperTwo,
       },
 });
 const swiperThree = new Swiper('.swiper_3', {
     watchSlidesProgress: true,
-    slidesPerView: 4,
+    slidesPerView: 5,
     loop: true,
-   
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+      },
 });
